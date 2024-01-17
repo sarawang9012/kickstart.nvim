a65248b86251c04a0d70799904cd849e3ce47dfe
sarawang9012 <625504790@qq.com>
2024-01-17 09:18:25 +0800

    added neotree and change default workspace of neorg to personal

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,39 +1,39 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  'nvim-neorg/neorg',
+  build = ':Neorg sync-parsers',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local neorg = require("neorg")
-    neorg.setup({
+    local neorg = require 'neorg'
+    neorg.setup {
       load = {
-        ["core.defaults"] = {},
-        ["core.summary"] = {},
-        ["core.concealer"] = {
+        ['core.defaults'] = {},
+        ['core.summary'] = {},
+        ['core.concealer'] = {
           config = {
             folds = false,
-            icon_preset = "diamond",
+            icon_preset = 'diamond',
           },
         },
-        ["core.completion"] = {
+        ['core.completion'] = {
           config = {
-            engine = "nvim-cmp",
-            name = "[Neorg]",
+            engine = 'nvim-cmp',
+            name = '[Neorg]',
           },
         },
-        ["core.dirman"] = {
+        ['core.dirman'] = {
           config = {
-            default_workspace = "notes",
-            index = "index.norg",
+            default_workspace = 'personal',
+            index = 'inbox.norg',
             workspaces = {
-              notes = "~/notes",
-              personal = "~/personal",
-              work = "~/work",
+              notes = '~/notes',
+              personal = '~/personal',
+              work = '~/work',
             },
           },
         },
       },
-    })
-    vim.cmd("set conceallevel=2")
-    vim.g.maplocalleader = ","
+    }
+    vim.cmd 'set conceallevel=2'
+    vim.g.maplocalleader = ','
   end,
 }
